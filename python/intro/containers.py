d111b33e48ed71ca83bd5acf3bb03a1c350d6573
mcclurgm <mcgearboulder@gmail.com>
2018-02-03 20:32:00 -0600

    Add Stack, HeaderBar, FlowBox, Notebook

--- python/intro/containers.py
+++ python/intro/containers.py
@@ -1,6 +1,6 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk, Gio
 
 class BoxWindow(Gtk.Window):
     def __init__(self):
@@ -113,7 +113,195 @@ class ListBoxWindow(Gtk.Window):
         box_outer.pack_start(listbox2, True, True, 0)
         listbox2.show_all()
 
-win = ListBoxWindow()
+class StackWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Stacks")
+        self.set_border_width(10)
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+
+        stack = Gtk.Stack()
+        stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
+        # stack.set_transition_duration(100)
+
+        checkbutton = Gtk.CheckButton("Click me")
+        stack.add_titled(checkbutton, "check", "Check Button")
+
+        label = Gtk.Label()
+        label.set_markup("<big>A Fancy Label</big>")
+        stack.add_titled(label, "label", "Label")
+
+        stack_switcher = Gtk.StackSwitcher()
+        stack_switcher.set_stack(stack)
+        vbox.pack_start(stack_switcher, False, False, 0)
+        vbox.pack_start(stack, True, True, 0)
+
+class HeaderBarWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="HeaderBar")
+        self.set_border_width(10)
+        self.set_default_size(400, 200)
+
+        hb = Gtk.HeaderBar()
+        hb.set_show_close_button(True)
+        hb.props.title = "HeaderBar example"
+        self.set_titlebar(hb)
+
+        button = Gtk.Button()
+        icon = Gio.ThemedIcon(name="mail-send-receive-symbolic")
+        image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
+        button.add(image)
+        hb.pack_end(button)
+
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        Gtk.StyleContext.add_class(box.get_style_context(), "linked")
+
+        button = Gtk.Button()
+        button.add(Gtk.Arrow(Gtk.ArrowType.LEFT, Gtk.ShadowType.NONE))
+        box.add(button)
+
+        button = Gtk.Button()
+        button.add(Gtk.Arrow(Gtk.ArrowType.RIGHT, Gtk.ShadowType.NONE))
+        box.add(button)
+        hb.pack_start(box)
+
+class FlowBoxWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="FlowBox")
+        self.set_border_width(10)
+        self.set_default_size(300, 250)
+
+        header = Gtk.HeaderBar(title="FlowBox")
+        header.set_subtitle("Sample FlowBox app")
+        header.props.show_close_button = True
+
+        self.set_titlebar(header)
+
+        scrolled = Gtk.ScrolledWindow()
+        scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+
+        flowbox = Gtk.FlowBox()
+        flowbox.set_valign(Gtk.Align.START)
+        flowbox.set_max_children_per_line(30)
+        flowbox.set_selection_mode(Gtk.SelectionMode.NONE)
+
+        self.create_flowbox(flowbox)
+
+        scrolled.add(flowbox)
+
+        self.add(scrolled)
+        self.show_all()
+
+    def color_swatch_new(self, str_color):
+        color = Gdk.color_parse(str_color)
+
+        rgba = Gdk.RGBA.from_color(color)
+        button = Gtk.Button()
+
+        area = Gtk.DrawingArea()
+        area.set_size_request(24, 24)
+        area.override_background_color(0, rgba)
+
+        button.add(area)
+
+        return button
+
+    def create_flowbox(self, flowbox):
+        colors = [
+        'AliceBlue',
+        'AntiqueWhite',
+        'AntiqueWhite1',
+        'AntiqueWhite2',
+        'AntiqueWhite3',
+        'AntiqueWhite4',
+        'aqua',
+        'aquamarine',
+        'aquamarine1',
+        'aquamarine2',
+        'aquamarine3',
+        'aquamarine4',
+        'azure',
+        'azure1',
+        'azure2',
+        'azure3',
+        'azure4',
+        'beige',
+        'bisque',
+        'bisque1',
+        'bisque2',
+        'bisque3',
+        'bisque4',
+        'black',
+        'BlanchedAlmond',
+        'blue',
+        'blue1',
+        'blue2',
+        'blue3',
+        'blue4',
+        'BlueViolet',
+        'brown',
+        'brown1',
+        'brown2',
+        'brown3',
+        'brown4',
+        'burlywood',
+        'burlywood1',
+        'burlywood2',
+        'burlywood3',
+        'burlywood4',
+        'CadetBlue',
+        'CadetBlue1',
+        'CadetBlue2',
+        'CadetBlue3',
+        'CadetBlue4',
+        'chartreuse',
+        'chartreuse1',
+        'chartreuse2',
+        'chartreuse3',
+        'chartreuse4',
+        'chocolate',
+        'chocolate1',
+        'chocolate2',
+        'chocolate3',
+        'chocolate4',
+        'coral',
+        'coral1',
+        'coral2',
+        'coral3',
+        'coral4'
+        ]
+
+        for color in colors:
+            button = self.color_swatch_new(color)
+            flowbox.add(button)
+
+class NotebookWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Notebook")
+        self.set_border_width(3)
+
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
+
+        self.page1 = Gtk.Box()
+        self.page1.set_border_width(10)
+        self.page1.add(Gtk.Label('Default Page!'))
+        self.notebook.append_page(self.page1, Gtk.Label('Plain Title'))
+
+        self.page2 = Gtk.Box()
+        self.page2.set_border_width(10)
+        self.page2.add(Gtk.Label('A page with an image for a Title.'))
+        self.notebook.append_page(
+            self.page2,
+            Gtk.Image.new_from_icon_name(
+                "help-about",
+                Gtk.IconSize.MENU
+            )
+        )
+
+win = NotebookWindow()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
